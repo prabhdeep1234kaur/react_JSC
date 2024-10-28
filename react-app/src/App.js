e4d01eb5022d15dcf0667d96aa0a601f0c5218b2
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-10-28 18:15:19 +0530

    > Form and submitting form > LocalStorage > Handling input data and search data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,28 +1,32 @@
 import { useState } from "react";
 import Header from './Header';
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
 import Content from './Content';
 import Footer from './Footer';
 function App() {
 
   //data taken from content js
-  const[items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: 'Beige Shoes from Call It Spring'
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'Maroon handbag from Coach'
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Sunglasses from Prada'
-    }
-  ]);//not directly changing it
+  const[items, setItems] = useState(JSON.parse(localStorage.getItem('Shoppinglist')));
 
+  const[search, setSearch] = useState("");
+
+  //setting and savinging items
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('Shoppinglist', JSON.stringify(newItems));
+  }
+
+  //for form 
+  const [newItem, setNewItem] = useState ("");
+
+  //adding item
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id+1 : 1;
+    const myNewItem = {id, checked: false, item};
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems);
+  }
 
   //drilling functions too
   const handleCheck = (id) => { //arrow func
@@ -30,25 +34,40 @@ function App() {
       ...item, 
       checked: !item.checked //make it the opposite of what is being in arr
     } : item);
-    setItems(listItems); //changing the state (of "checked value in array")
-    localStorage.setItem('Shoppinglist', JSON.stringify(listItems)); //saved under Shoppinglist in our local storage
-
+    setAndSaveItems(listItems);
   }
 
   const handleDelete = (id) => {
     const newList = items.filter(
       (item)=>item.id !== id
     ); //clears new arr of filtered items which is not equal to id we sent, so removed the item
-    setItems(newList);
-    localStorage.setItem('Shoppinglist', JSON.stringify(newList));
+    setAndSaveItems(newList);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!newItem) return;
+    //add item funct
+    addItem(newItem);
+    setNewItem("");//empty the input
+   
   }
 
   return (
     <div className="App">
 
       <Header title="Shopping List"/>
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
+      <AddItem 
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content 
-        items={items} //being passed to content
+        items={items.filter(item => (item.item).toLowerCase().includes(search.toLocaleLowerCase()))} //being passed to content
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
