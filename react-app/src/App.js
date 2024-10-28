f16293b7e1f2588a0bcc5b84b124379973c2548b
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2024-10-28 16:08:10 +0530

    Props and Prop Drilling 1. Parent to child data passing via Properties (Prop)
    App.js -> Content.js -> ItemList.js -> Footer.js
    
    2. Parent to children (Among siblings) data passing| Drilling via properties

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,58 @@
+import { useState } from "react";
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 function App() {
-  
+
+  //data taken from content js
+  const[items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: 'Beige Shoes from Call It Spring'
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Maroon handbag from Coach'
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Sunglasses from Prada'
+    }
+  ]);//not directly changing it
+
+
+  //drilling functions too
+  const handleCheck = (id) => { //arrow func
+    const listItems = items.map((item)=>item.id === id ? {
+      ...item, 
+      checked: !item.checked //make it the opposite of what is being in arr
+    } : item);
+    setItems(listItems); //changing the state (of "checked value in array")
+    localStorage.setItem('Shoppinglist', JSON.stringify(listItems)); //saved under Shoppinglist in our local storage
+
+  }
+
+  const handleDelete = (id) => {
+    const newList = items.filter(
+      (item)=>item.id !== id
+    ); //clears new arr of filtered items which is not equal to id we sent, so removed the item
+    setItems(newList);
+    localStorage.setItem('Shoppinglist', JSON.stringify(newList));
+  }
+
   return (
     <div className="App">
 
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Shopping List"/>
+      <Content 
+        items={items} //being passed to content
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
+      <Footer length={items.length} />
     </div>
   );
 }
